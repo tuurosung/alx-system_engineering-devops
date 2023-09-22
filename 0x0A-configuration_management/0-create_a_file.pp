5eb0f87323aa048e64e9d223112f944b4a19098a
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-09-22 23:01:45 +0000

    solve 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #creates a temporary file
 
 file { 'school':
-    ensure	=> 'present',
-    content	=> 'I love Puppet',
-    mode	=> '0744',
-    owner	=> 'www-data',
-    group	=> 'www-data',
-    path	=> '/tmp/school',
+ensure  => 'present',
+content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+path    => '/tmp/school',
 }
