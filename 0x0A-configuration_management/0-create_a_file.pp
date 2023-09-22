b96343740610afe46115e7d878506c5238d07ed0
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-09-22 22:31:20 +0000

    Solve 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
 #creates a temporary file the /tmp dir
-file { '/tmp/school':
+
+file { 'school':
+    ensure => 'present',
     content => 'I love Puppet',
     mode => '0744',
     owner => 'www-data',
     group => 'www-data',
+    path => '/tmp/school',
 }
